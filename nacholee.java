9869bacb83deedc5b609fe92e1cca00b08a2eb45
Sala K 405 <salak405@salasfr.itm.edu.co>
2026-02-09 09:10:26 -0500

    Suma de numeros

--- nacholee.java
+++ nacholee.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class nacholee {
 
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Vamos a sumar 2 numeros");
+        int n1=0, n2=0, suma=0;
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese el numero 1: ");
+        n1 = teclado.nextInt();
+        System.out.println("Ingrese el numero 2: ");
+        n2 = teclado.nextInt();
+
+        suma = n1 + n2;
+
+        System.out.println("La suma de los 2 numeros es: " + suma);
     }
 }
